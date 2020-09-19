1c8392fbd6e2e05680cad8fe5af5902ce20885ea
dhruvi4200 <60963665+dhruvi4200@users.noreply.github.com>
2020-09-19 13:46:17 +0530

    Prac3d
    Factorial function

--- Prac3d.py
+++ Prac3d.py
@@ -1,57 +1,40 @@
-class Fact:
-    def __init__(self, number):
-        self.number = number
+for i in range(1,n+1):
+    factorial = factorial * i
 
-    def factorial_iteration(self):
-        factorial = 1
-        if self.number < 0:
-            raise Exception("Factorial of Negative number doesn't exist")
-        elif self.number == 0:
-            return factorial
-        else:
-            for i in range(1, self.number + 1):
-                factorial = factorial * i
-            return factorial
+print(f'Factorial is : {factorial}')
 
-    def factorial_recursion(self, number=None):
-        if number is None:
-            number = self.number
-        if number < 0:
-            raise Exception("Factorial of Negative number doesn't exist")
-        elif number == 0 or number == 1:
-            return 1
-        else:
-            return number * self.factorial_recursion(number - 1)
+fact = []
+for i in range(1,n+1):
+    if (n/i).is_integer():
+        fact.append(i)
+        
+print(f'Factors of the given numbers is : {fact}')
 
-    def factors_iteration(self):
-        factors = []
-        for i in range(1, self.number + 1):
-            if self.number % i == 0:
-                factors.append(i)
-        return factors
+factorial = 1
+index = 1
+n = int(input("Enter number : "))
+def calculate_factorial(n,factorial,index):
+    if index == n:
+        print(f'Factorial is : {factorial}')
+        return True
+    else:
+        index = index + 1
+        calculate_factorial(n,factorial * index,index)
+calculate_factorial(n,factorial,index)
 
-    def factors_recursion(self, number=None, lst=None):
-        factorial = self.number
-        if number is None and lst is None:
-            number = self.number
-            lst = []
-        if number == 0 or number < 0:
-            return lst
-        elif number == 1:
-            lst.append(1)
-            return sorted(lst)
-        elif factorial % number == 0:
-            lst.append(number)
-            return self.factors_recursion(number - 1, lst)
-        else:
-            return self.factors_recursion(number - 1, lst)
-
-
-if __name__ == '__main__':
-    fact = Fact(int(input("Enter a number: ")))
-    print(fact.factorial_iteration())
-    print(fact.factorial_recursion())
-    print(fact.factors_iteration())
-    print(fact.factors_recursion())
-
-   
+fact = []
+def calculate_factors(n,factors,index):
+    if index == n+1:
+        print(f'Factors of the given numbers is : {factors}')
+        return True
+    elif (n/index).is_integer():
+        factors.append(index)
+        index += 1
+        calculate_factors(n,factors,index)
+    else:
+        index += 1
+        calculate_factors(n,factors,index)
+        
+index = 1
+factors = []
+calculate_factors(n,factors,index)
